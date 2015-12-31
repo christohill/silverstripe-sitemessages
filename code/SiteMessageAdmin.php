1327e9e44453de8c65820939d6955561963d6b15
helpfulrobot <chris@silverstripe.com>
2016-01-01 03:37:08 +1300

    Converted to PSR-2

--- code/SiteMessageAdmin.php
+++ code/SiteMessageAdmin.php
@@ -1,39 +1,40 @@
 <?php
 
-class SiteMessageAdmin extends ModelAdmin implements PermissionProvider {
-	
-	private static $managed_models = array("SiteMessage");
-	private static $model_importers = array();
-	private static $url_segment = "sitemessages";
-	private static $menu_title = "Site Messages";
+class SiteMessageAdmin extends ModelAdmin implements PermissionProvider
+{
+    
+    private static $managed_models = array("SiteMessage");
+    private static $model_importers = array();
+    private static $url_segment = "sitemessages";
+    private static $menu_title = "Site Messages";
     
     /**
      * Add site message CRUD to permissions list for Members
      * @return NULL
      */
-	function providePermissions(){
+    public function providePermissions()
+    {
         return array(
             "SITEMESSAGES_VIEW" => array(
-            	"category" => _t("SiteMessage.PERMISSION_TITLE", "Site Messages"),
+                "category" => _t("SiteMessage.PERMISSION_TITLE", "Site Messages"),
                 "name" => _t("SiteMessage.VIEW_NAME", "View site messages"),
-            	"help" => _t("SiteMessage.VIEW_HELP", "Can view site messages in the CMS")
+                "help" => _t("SiteMessage.VIEW_HELP", "Can view site messages in the CMS")
             ),
             "SITEMESSAGES_CREATE" => array(
-            	"category" => _t("SiteMessage.PERMISSION_TITLE", "Site Messages"),
+                "category" => _t("SiteMessage.PERMISSION_TITLE", "Site Messages"),
                 "name" => _t("SiteMessage.CREATE_NAME", "Create site messages"),
-            	"help" => _t("SiteMessage.CREATE_HELP", "Can create site messages in the CMS")
+                "help" => _t("SiteMessage.CREATE_HELP", "Can create site messages in the CMS")
             ),
             "SITEMESSAGES_EDIT" => array(
-            	"category" => _t("SiteMessage.PERMISSION_TITLE", "Site Messages"),
+                "category" => _t("SiteMessage.PERMISSION_TITLE", "Site Messages"),
                 "name" => _t("SiteMessage.EDIT_NAME", "Edit site messages"),
-            	"help" => _t("SiteMessage.EDIT_HELP", "Can edit site messages in the CMS")
+                "help" => _t("SiteMessage.EDIT_HELP", "Can edit site messages in the CMS")
             ),
             "SITEMESSAGES_DELETE" => array(
-            	"category" => _t("SiteMessage.PERMISSION_TITLE", "Site Messages"),
+                "category" => _t("SiteMessage.PERMISSION_TITLE", "Site Messages"),
                 "name" => _t("SiteMessage.DELETE_NAME", "Delete site messages"),
-            	"help" => _t("SiteMessage.DELETE_HELP", "Can delete site messages in the CMS")
+                "help" => _t("SiteMessage.DELETE_HELP", "Can delete site messages in the CMS")
             )
         );
     }
-    
 }
--- code/SiteMessageValidator.php
+++ code/SiteMessageValidator.php
@@ -1,42 +1,41 @@
 <?php
 
-class SiteMessageValidator extends RequiredFields {
-	
-	/**
-	 * Custom validation for the SiteMessage CMS form
-	 * @param  Mixed	$data
-	 * @return Boolean Returns TRUE/FALSE based on errors found in the validator
-	 */
-	function php($data) {
-		
-		$valid = TRUE;
-		
-		// If there is button text but no page to link to
-		if($data['ButtonText'] && !$data['PageID']) {
-			$this->validationError(
-				"PageID",
-				_t("SiteMessage.PAGEIDERROR", "Please choose a page to link to or remove the button text."),
-				"error"
-			);
-			
-			// Return false
-			$valid = FALSE;
-		}
-		
-		// If there is no button text but a page to link to
-		if(!$data['ButtonText'] && $data['PageID']) {
-			$this->validationError(
-				"ButtonText",
-				_t("SiteMessage.BUTTONTEXTERROR", "Please enter button text or remove the page to link to."),
-				"error"
-			);
-			
-			// Return false
-			$valid = FALSE;
-		}
-		
-		return $valid;
-		
-	}
-	
+class SiteMessageValidator extends RequiredFields
+{
+    
+    /**
+     * Custom validation for the SiteMessage CMS form
+     * @param  Mixed	$data
+     * @return Boolean Returns TRUE/FALSE based on errors found in the validator
+     */
+    public function php($data)
+    {
+        $valid = true;
+        
+        // If there is button text but no page to link to
+        if ($data['ButtonText'] && !$data['PageID']) {
+            $this->validationError(
+                "PageID",
+                _t("SiteMessage.PAGEIDERROR", "Please choose a page to link to or remove the button text."),
+                "error"
+            );
+            
+            // Return false
+            $valid = false;
+        }
+        
+        // If there is no button text but a page to link to
+        if (!$data['ButtonText'] && $data['PageID']) {
+            $this->validationError(
+                "ButtonText",
+                _t("SiteMessage.BUTTONTEXTERROR", "Please enter button text or remove the page to link to."),
+                "error"
+            );
+            
+            // Return false
+            $valid = false;
+        }
+        
+        return $valid;
+    }
 }
